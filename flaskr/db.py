61daeca5b9076b4170472a4a197454652131f16b
nbosio1001 <n.bosio@yahoo.com>
2021-02-18 17:48:46 -0800

    Hid some comments in test_company_list

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,7 +3,6 @@ import sqlite3
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
-from flask_sqlalchemy import SQLAlchemy
 
 
 def get_db():
@@ -29,8 +28,7 @@ def close_db(e=None):
         db.close()
 
 def init_db():
-    # db = get_db()
-    db = SQLAlchemy()
+    db = get_db()
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
