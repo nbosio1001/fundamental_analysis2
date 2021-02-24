e3f03c04abe835bbf378d7ef51bae4da49a28fa9
nbosio1001 <n.bosio@yahoo.com>
2021-02-23 17:51:32 -0800

    Running App, able to remove from database

--- flaskr/company_list.py
+++ flaskr/company_list.py
@@ -12,7 +12,7 @@ bp = Blueprint('company_list', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT ticker_symbol FROM post ORDER BY ticker_symbol ASC'
+        'SELECT id,ticker_symbol FROM post ORDER BY ticker_symbol ASC'
     ).fetchall()
     return render_template('company_list/index.html', posts=posts)
 
@@ -57,73 +57,29 @@ def create():
     return render_template('company_list/create.html')
 
 
-# def get_post(id, check_author=True):
-#     post = get_db().execute(
-#         #'SELECT id, ticker_symbol, body, created, author_id'#, username'
-#         'SELECT id, ticker_symbol, created, author_id'
-#         ' FROM post'# p JOIN user u ON p.author_id = u.id'
-#         ' WHERE id = ?',
-#         (id,)
-#     ).fetchone()
+def get_post(id):
+    post = get_db().execute(
+        'SELECT id, ticker_symbol'
+        ' FROM post'
+        ' WHERE id = ?',
+        (id,)
+    ).fetchone()
 
-#     if post is None:
-#         abort(404, "Post id {0} doesn't exist.".format(id))
-
-    # if check_author and post['author_id'] != g.user['id']:
-    #     abort(403)
-
-    # return post
-
-# @bp.route('/<int:id>/update', methods=('GET', 'POST'))
-# # @login_required
-# def update(id):
-#     post = get_post(id)
-
-#     if request.method == 'POST':
-#         ticker_symbol = request.form['ticker_symbol']
-#         # body = request.form['body']
-#         error = None
-
-#         if not ticker_symbol:
-#             error = 'Ticker Symbol is required.'
-
-#         if error is not None:
-#             flash(error)
-#         else:
-#             db = get_db()
-#             db.execute(
-#                 'UPDATE post SET ticker_symbol = ?'#, body = ?'
-#                 ' WHERE id = ?',
-#                 (ticker_symbol, body, id)
-#             )
-#             db.commit()
-#             return redirect(url_for('company_list.index'))
-
-#     return render_template('company_list/update.html', post=post)
-
-# @bp.route('/<int:id>/delete', methods=('POST',))
-# # @login_required
-# def delete(id):
-#     get_post(id)
-#     db = get_db()
-#     db.execute('DELETE FROM post WHERE id = ?', (id,))
-#     db.commit()
-#     return redirect(url_for('company_list.index'))
-
-# _________________________________________________________
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
 
 
+    return post           
 
 
-
-
-# @bp.before_app_request
-# def load_logged_in_user():
-#     user_id = session.get('user_id')
-
-#     if user_id is None:
-#         g.user = None
-#     else:
-#         g.user = get_db().execute(
-#             'SELECT * FROM user WHERE id = ?', (user_id,)
-#         ).fetchone()
+@bp.route('/<int:id>/delete', methods=('POST','GET'))
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute(
+        'DELETE FROM post'
+        ' WHERE id = ?',
+        (id,)
+    )
+    db.commit()
+    return redirect(url_for('company_list.index'))
--- tests/data.sql
+++ tests/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO post (ticker_symbol, author_id, created)-- body, )
+INSERT INTO post (ticker_symbol)--, author_id, created)-- body, )
 VALUES
-  ('TSLA', 1, '2018-01-01 00:00:00'); --  'Tesla, Inc.',
+  ('TSLA')--, 1, '2018-01-01 00:00:00'); --  'Tesla, Inc.',
