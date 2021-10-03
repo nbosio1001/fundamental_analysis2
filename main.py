f8c94004de6fc8a8f7da8558fada962c6176c49f
nbosio1001 <n.bosio@yahoo.com>
2021-10-03 03:34:35 -0700

    Added requests_cache to store requests data temporarily in SQLlite db. This is instead of sending a POST request every time to server.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
+import requests_cache
 import pandas as pd
 from pandas .io.json import json_normalize
 from bs4 import BeautifulSoup
 import pprint
 import json
 
+
+requests_cache.install_cache()
 # def JSON_Request():
 TESLA_CIK = "0001318605"
 html_website = "https://data.sec.gov/submissions/CIK{}.json".format(TESLA_CIK)
 response = requests.get(html_website, headers={'User-Agent': 'Mozilla/5.0'})
+print(response.from_cache)
 json_response = response.json()
 # print(filings)
 df = pd.DataFrame.from_dict(json_response["filings"]["recent"])
@@ -23,8 +27,8 @@ print(company_10ks["link"].head())
 
 # Website we want https://www.sec.gov/cgi-bin/viewer?action=view&cik=1318605&accession_number=0001564590-21-004599&xbrl_type=v
 
-https://data.sec.gov/api/xbrl/companyfacts/CIK0001318605.json
+# https://data.sec.gov/api/xbrl/companyfacts/CIK0001318605.json
 
-API endpoint, specific request,
+# API endpoint, specific request,
 
-line 1614
+# line 1614
